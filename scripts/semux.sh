63e056b239dc83185024233598f7fbcb2c34c276
lodyman93 <lodywork@gmail.com>
2017-11-01 21:13:15 +0200

    Update semux.sh
    Enabling java version checking

--- scripts/semux.sh
+++ scripts/semux.sh
@@ -3,5 +3,19 @@
 # change work directory
 cd "$(dirname "$0")"
 
+# check if java is installed
+if ! [ -x "$(command -v java)" ]; then
+    echo "Error: Java is not installed"
+    exit 1
+fi
+# check if java version is correct
+version=$(java -version 2>&1 | egrep -o '[1-2].[6-9]' )
+min=$(1.8)
+
+    if [ "$version" =< "$min" ]; then
+        echo "Error: Java 8 or above is required"
+    exit 1
+fi
+
 # start kernel
 java -cp "./config:./lib/*" org.semux.Semux $@
